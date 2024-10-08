5f28b5dee2a970cc048739ad6ea570f0e9fc04fc
NotmyMagic <56935337+NotmyMagic@users.noreply.github.com>
2024-10-08 14:00:19 -0600

    added comment

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ function gameEnd() {
   });
   cards.forEach((card) => card.addEventListener("click", flipCard));
 
+  //reshuffle
   cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
